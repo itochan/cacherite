e15962acf659cd299ce022e80af9facfde16e4f0
Kazunori Jo (itochan) <itochan315@gmail.com>
2012-04-24 15:54:15 +0900

    basic functions

---
+++ lib/cacherite.rb
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+class Cacherite
+  attr_accessor :lifetime
+
+  def initialize(directory)
+    @directory = File.expand_path(directory)
+    @lifetime = 3600
+  end
+
+  def get(key)
+    begin
+      cache = open(File.join(@directory, key)).read
+      if Time.now - File::Stat.new(File.join(@directory, key)).mtime >= @lifetime
+        self.remove(key)
+        nil
+      else
+        @key = key
+        cache
+      end
+    rescue Errno::ENOENT
+      nil
+    end
+  end
+
+  def save(key, content)
+    cache = File.open(File.join(@directory, key), "w")
+    cache.write(content)
+    cache.close
+
+    true
+  end
+
+  def remove(key)
+    FileUtils.rm(File.join(@directory, key))
+  end
+
+  def clean
+    FileUtils.rm(Dir.glob(File.join(@directory, "*")))
+  end
+
+  def extend_life(key = @key)
+    begin
+      FileUtils.touch(File.join(@directory, key))
+    rescue TypeError
+      raise "key has not been set or is not get"
+    end
+
+    File::Stat.new(File.join(@directory, key)).mtime
+  end
+end
--- spec/cacherite_spec.rb
+++ spec/cacherite_spec.rb
@@ -1,7 +1,55 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Cacherite" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Cacherite do
+  before do
+    @directory = File.join(File.dirname(__FILE__), "tmp")
+    @cr = Cacherite.new(@directory)
+    @cr.clean
+  end
+
+  it "set lifetime" do
+    @cr.lifetime = 60
+  end
+
+  it "get cache as nil" do
+    @cr.get("key").should be_nil
+  end
+
+  it "caching" do
+    @cr.save("key", "content")
+  end
+
+  it "hit cache" do
+    @cr.save("key", "content")
+    @cr.get("key").should eq "content"
+  end
+
+  it "missing cache" do
+    @cr.save("key", "content")
+    @cr.lifetime = 0
+    @cr.get("key").should be_nil
+    @cr.lifetime = 3600
+  end
+
+  it "cache remove" do
+    @cr.save("key", "content")
+    @cr.remove("key")
+    @cr.get("key").should be_nil
+  end
+
+  it "cache clean" do
+    @cr.save("key", "content")
+    @cr.clean
+    @cr.get("key").should be_nil
+  end
+
+  it "extend life" do
+    @cr.save("key", "content")
+    old = File::Stat.new(File.join(@directory, "key")).mtime
+    @cr.get("key")
+    sleep 1
+    @cr.extend_life().should_not be_false
+    new = File::Stat.new(File.join(@directory, "key")).mtime
+    (new - old).should eq 1
   end
 end
